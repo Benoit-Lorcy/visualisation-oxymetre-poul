178d72fb6270b5b597cd2d0a39d78090afc25e1c
MathieuRoux <mathieu.roux222@gmail.com>
2022-04-04 17:53:41 +0200

    Mesure fonctionne, sauf conversion ratio => sp02

--- Minimal_CIR3/main.c
+++ Minimal_CIR3/main.c
@@ -33,21 +33,21 @@ int main()
     return EXIT_SUCCESS;
 }
 
-void test_UART()
-{
-    void *handler = init_UART();
-    int state;
+// void test_UART()
+// {
+//     void *handler = init_UART();
+//     int state;
 
-    int count = 0;
-    while (++count < 50)
-    {
-        absorp data = read_UART(handler, &state);
+//     int count = 0;
+//     while (++count < 50)
+//     {
+//         absorp data = read_UART(handler, &state);
 
-        if (state == EOF)
-        {
-            printf("EOF!\n");
-            break;
-        }
-        printf("Struct : %f %f %f %f\n", data.acr, data.dcr, data.acir, data.dcir);
-    }
-}
+//         if (state == EOF)
+//         {
+//             printf("EOF!\n");
+//             break;
+//         }
+//         printf("Struct : %f %f %f %f\n", data.acr, data.dcr, data.acir, data.dcir);
+//     }
+// }
--- Minimal_CIR3/mesure.c
+++ Minimal_CIR3/mesure.c
@@ -1,31 +1,109 @@
 #include "mesure.h"
 
+#include <math.h>
+
 #include "fichiers.h"
 
-oxy mesureTest(char* filename){
+// Value to be above or under to qualify to be able to be a new 0
+#define VALUE_TO_CLEAR 10
+
+oxy mesureTest(char *filename)
+{
 	oxy myOxy;
 
-	FILE* f = initFichier(filename);
+	FILE *f = initFichier(filename);
 
-	oxy un, deux;
+	oxy current;
+	int file_state = 1;
 
-	float r_max = 0, r_min = 0;
-	float ir_max = 0, ir_min = 0;
+	// State variables
 
+	char state = 0; // 0 : first value, 1 : Search for first 0, 2 : Continue search for zeroes
 
-	int state = 1;
-	do {
-		absorp data = lireFichier(f, &state);
+	float zero_count = 0;
+	char can_be_zero = 1;
 
-		if(data.acr > r_max) r_max = data.acr;
-		if(data.acir > ir_max) r_max = data.acir;
-		if(data.acr < r_min) r_min = data.acr;
-		if(data.acir < ir_min) ir_min = data.acir;
-	} while(state != EOF);
+	float r_maximum, r_minimum;
+	float ir_maximum, ir_minimum;
+	float r_dc_average = 0;
+	float ir_dc_average = 0;
 
-	printf("Yo2 : %f, %f\n", r_max, r_min);
+	long counter = 0;
+
+	absorp previous;
+
+	float BPM[8] = {70, 70, 70, 70, 70, 70, 70, 70};
+	float RsIR[8] = {1,1,1,1,1,1,1,1};
+	int BPM_index = 0;
+
+	do
+	{
+		absorp data = lireFichier(f, &file_state);
+		counter++;
+
+		switch (state)
+		{
+		case 0:
+			state = 1;
+			break;
+		case 1:
+			if ((previous.acr > 0 && data.acr <= 0) || (previous.acr < 0 && data.acr >= 0))
+			{
+				state = 2;
+				counter = 0;
+				zero_count = 1;
+				can_be_zero = 0;
+			}
+			break;
+		case 2:
+			if (can_be_zero && (((previous.acr > 0 && data.acr <= 0) || (previous.acr < 0 && data.acr >= 0))))
+			{
+				zero_count++;
+				can_be_zero = 0;
+
+				if (zero_count == 3)
+				{
+					// Full period
+					BPM[BPM_index] = 30000.0f / counter;
+					RsIR[BPM_index] = ir_dc_average * (r_maximum = r_minimum) / (r_dc_average * (ir_maximum - ir_minimum));
+					BPM_index = (BPM_index + 1) % 8;
+
+					zero_count = 1;
+					r_dc_average = data.dcr;
+					ir_dc_average = data.dcir;
+					r_maximum = r_minimum = ir_maximum = ir_minimum = 0;
+					counter = 1;
+				}
+			}
+
+			if (data.acr > VALUE_TO_CLEAR || data.acr < VALUE_TO_CLEAR)
+			{
+				can_be_zero = 1;
+			}
+			if (data.acr > r_maximum)
+				r_maximum = data.acr;
+			else if (data.acr < r_minimum)
+				r_minimum = data.acr;
+			if (data.acir > ir_maximum)
+				ir_maximum = data.acir;
+			else if (data.acir < ir_minimum)
+				ir_minimum = data.acir;
+
+			r_dc_average = (r_dc_average + data.dcr) / 2.0f;
+			ir_dc_average = (ir_dc_average + data.dcir) / 2.0f;
+			break;
+
+		default:
+			break;
+		}
+		previous = data;
+	} while (file_state != EOF);
+
+	BPM[0] = (BPM[0] + BPM[1] + BPM[2] + BPM[3] + BPM[4] + BPM[5] + BPM[6] + BPM[7]) / 8.0f;
+	RsIR[0] = (RsIR[0] + RsIR[1] + RsIR[2] + RsIR[3] + RsIR[4] + RsIR[5] + RsIR[6] + RsIR[7]) / 8.0f;
+	
+	printf("BPM : %d\n", (int)BPM[0]);
+	//printf()
 
 	return myOxy;
-
 }
-
