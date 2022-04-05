b679bf97866970b737712f0b19f4b09e87c917a4
MathieuRoux <mathieu.roux222@gmail.com>
2022-04-05 09:54:18 +0200

    Mesure propre

--- Minimal_CIR3/mesure.c
+++ Minimal_CIR3/mesure.c
@@ -1,6 +1,8 @@
 #include "mesure.h"
 
 #include <math.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "fichiers.h"
 
@@ -13,100 +15,126 @@ oxy mesureTest(char *filename)
 
 	FILE *f = initFichier(filename);
 
-	oxy current;
 	int file_state = 1;
 
-	// State variables
-
-	char state = 0; // 0 : first value, 1 : Search for first 0, 2 : Continue search for zeroes
-
-	float zero_count = 0;
-	char can_be_zero = 1;
-
-	float r_maximum, r_minimum;
-	float ir_maximum, ir_minimum;
-	float r_dc_average = 0;
-	float ir_dc_average = 0;
-
-	long counter = 0;
-
-	absorp previous;
-
-	float BPM[8] = {70, 70, 70, 70, 70, 70, 70, 70};
-	float RsIR[8] = {1,1,1,1,1,1,1,1};
-	int BPM_index = 0;
-
+	MesureEnv *env = mesure_init();
 	do
 	{
 		absorp data = lireFichier(f, &file_state);
-		counter++;
-
-		switch (state)
-		{
-		case 0:
-			state = 1;
-			break;
-		case 1:
-			if ((previous.acr > 0 && data.acr <= 0) || (previous.acr < 0 && data.acr >= 0))
-			{
-				state = 2;
-				counter = 0;
-				zero_count = 1;
-				can_be_zero = 0;
-			}
-			break;
-		case 2:
-			if (can_be_zero && (((previous.acr > 0 && data.acr <= 0) || (previous.acr < 0 && data.acr >= 0))))
-			{
-				zero_count++;
-				can_be_zero = 0;
-
-				if (zero_count == 3)
-				{
-					// Full period
-					BPM[BPM_index] = 30000.0f / counter;
-					RsIR[BPM_index] = ir_dc_average * (r_maximum - r_minimum) / (r_dc_average * (ir_maximum - ir_minimum));
-					BPM_index = (BPM_index + 1) % 8;
-
-					zero_count = 1;
-					r_dc_average = data.dcr;
-					ir_dc_average = data.dcir;
-					r_maximum = r_minimum = ir_maximum = ir_minimum = 0;
-					counter = 1;
-				}
-			}
-
-			if (data.acr > VALUE_TO_CLEAR || data.acr < VALUE_TO_CLEAR)
-			{
-				can_be_zero = 1;
-			}
-			if (data.acr > r_maximum)
-				r_maximum = data.acr;
-			else if (data.acr < r_minimum)
-				r_minimum = data.acr;
-			if (data.acir > ir_maximum)
-				ir_maximum = data.acir;
-			else if (data.acir < ir_minimum)
-				ir_minimum = data.acir;
-
-			r_dc_average = (r_dc_average + data.dcr) / 2.0f;
-			ir_dc_average = (ir_dc_average + data.dcir) / 2.0f;
-			break;
-
-		default:
-			break;
-		}
-		previous = data;
+		mesure(env, data);
 	} while (file_state != EOF);
 
-	myOxy.pouls = (BPM[0] + BPM[1] + BPM[2] + BPM[3] + BPM[4] + BPM[5] + BPM[6] + BPM[7]) / 8.0f;
-	float ratio = (RsIR[0] + RsIR[1] + RsIR[2] + RsIR[3] + RsIR[4] + RsIR[5] + RsIR[6] + RsIR[7]) / 8.0f;
+	return env->current;
+}
 
-	if(ratio > 1) {
-		myOxy.spo2 = (int) ((3.4f - ratio) * 50.0f / (3.4f - 2));
-	} else {
-		myOxy.spo2 = (int) ((0.8f - ratio) * 10.0f / (0.8f - 0.4f) + 90);
+MesureEnv *mesure_init()
+{
+	MesureEnv *env = malloc(sizeof(MesureEnv));
+
+	if (env == NULL)
+	{
+		// SAD :(
+		return NULL;
 	}
 
-	return myOxy;
+	memset(env, 0, sizeof(MesureEnv));
+	env->can_be_zero = 1;
+
+	int i = 0;
+	for (; i < 8; i++)
+	{
+		env->BPM[i] = 70;
+		env->RsIR[i] = 1;
+	}
+	env->current.pouls = 70;
+	env->current.spo2 = 85;
+}
+
+oxy mesure(MesureEnv *env, absorp data)
+{
+	env->counter++;
+
+	switch (env->mesure_state)
+	{
+	case 0:
+		env->mesure_state = 1;
+		break;
+	case 1:
+		if ((env->previous.acr > 0 && data.acr <= 0) || (env->previous.acr < 0 && data.acr >= 0))
+		{
+			env->mesure_state = 2;
+			env->counter = 0;
+			env->zero_count = 1;
+			env->can_be_zero = 0;
+		}
+		break;
+	case 2:
+		if (env->can_be_zero && (((env->previous.acr > 0 && data.acr <= 0) || (env->previous.acr < 0 && data.acr >= 0))))
+		{
+			env->zero_count++;
+			env->can_be_zero = 0;
+
+			if (env->zero_count == 3)
+			{
+				// Full period
+				env->BPM[env->BPM_index] = 30000.0f / env->counter;
+				env->RsIR[env->BPM_index] = env->ir_dc_average * (env->r_maximum - env->r_minimum) / (env->r_dc_average * (env->ir_maximum - env->ir_minimum));
+				env->BPM_index = (env->BPM_index + 1) % 8;
+
+				env->zero_count = 1;
+				env->r_dc_average = data.dcr;
+				env->ir_dc_average = data.dcir;
+				env->r_maximum = env->r_minimum = env->ir_maximum = env->ir_minimum = 0;
+				env->counter = 1;
+
+				env->current.pouls = (env->BPM[0] + env->BPM[1] + env->BPM[2] + env->BPM[3] + env->BPM[4] + env->BPM[5] + env->BPM[6] + env->BPM[7]) / 8.0f;
+				float ratio = (env->RsIR[0] + env->RsIR[1] + env->RsIR[2] + env->RsIR[3] + env->RsIR[4] + env->RsIR[5] + env->RsIR[6] + env->RsIR[7]) / 8.0f;
+
+				if (ratio > 1)
+				{
+					env->current.spo2 = (int)((3.4f - ratio) * 50.0f / (3.4f - 2));
+				}
+				else
+				{
+					env->current.spo2 = (int)((0.8f - ratio) * 10.0f / (0.8f - 0.4f) + 90);
+				}
+			}
+		}
+
+		if (data.acr > VALUE_TO_CLEAR || data.acr < VALUE_TO_CLEAR)
+		{
+			env->can_be_zero = 1;
+		}
+		if (data.acr > env->r_maximum)
+		{
+			env->r_maximum = data.acr;
+		}
+		else if (data.acr < env->r_minimum)
+		{
+			env->r_minimum = data.acr;
+		}
+		if (data.acir > env->ir_maximum)
+		{
+			env->ir_maximum = data.acir;
+		}
+		else if (data.acir < env->ir_minimum)
+		{
+			env->ir_minimum = data.acir;
+		}
+
+		env->r_dc_average = (env->r_dc_average + data.dcr) / 2.0f;
+		env->ir_dc_average = (env->ir_dc_average + data.dcir) / 2.0f;
+		break;
+
+	default:
+		break;
+	}
+	env->previous = data;
+
+	return env->current;
+}
+
+void mesure_close(MesureEnv *env)
+{
+	free(env);
 }
--- Minimal_CIR3/mesure.h
+++ Minimal_CIR3/mesure.h
@@ -1,5 +1,28 @@
 #include "define.h"
 
+typedef struct {
+    char mesure_state;
+    float zero_count;
+    char can_be_zero;
+    float r_maximum, r_minimum;
+    float ir_maximum, ir_minimum;
+
+    float r_dc_average;
+	float ir_dc_average;
+
+	long counter;
+
+	absorp previous;
+
+    float BPM[8];
+    float RsIR[8];
+    int BPM_index;
+
+    oxy current;
+} MesureEnv;
+
 oxy mesureTest(char* filename);
 
-	
+MesureEnv *mesure_init();
+oxy mesure(MesureEnv *env, absorp data);
+void mesure_close(MesureEnv *env);
